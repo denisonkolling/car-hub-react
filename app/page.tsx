ca12532eae1ead4f8bd28a5e44a452472b89c91b
Denison Kolling <denisonkolling@gmail.com>
2024-02-13 11:42:41 -0300

    update readme.md

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { fetchCars } from '@/utils'
 import CarCard from '@/components/CarCard'
 import { fuels, yearsOfProduction } from '@/constants'
 
-export default async function Home({searchParams}) {
+export default async function Home({searchParams}: {searchParams: any}) {
 
   const allCars = await fetchCars({
     manufacturer: searchParams.manufacturer || '',
